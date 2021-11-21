72bb428b1e35f5699405c0dbb36db96f8ae5bd2e
yuki-katayama <katayu810@gmail.com>
2021-11-21 09:55:19 +0900

    add: module06_ex02

---
+++ module06/ex02/main.cpp
@@ -0,0 +1,83 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+
+Base::~Base(void) {}
+
+void identify(Base &p)
+{
+	try
+	{
+		A &a = dynamic_cast<A &>(p);
+		static_cast<void>(a);
+		std::cout << "A" << std::endl;
+	}
+	catch (std::bad_cast err) {}
+	try
+	{
+		B &a = dynamic_cast<B &>(p);
+		static_cast<void>(a);
+		std::cout << "B" << std::endl;
+	}
+	catch (std::bad_cast err) {}
+	try
+	{
+		C &a = dynamic_cast<C &>(p);
+		static_cast<void>(a);
+		std::cout << "C" << std::endl;
+	}
+	catch (std::bad_cast err) {}
+}
+
+void identify(Base *p)
+{
+	if(dynamic_cast<A *>(p))
+	{
+		std::cout << "A" << std::endl;
+		return ;
+	}
+	if(dynamic_cast<B *>(p))
+	{
+		std::cout << "B" << std::endl;
+		return ;
+	}
+	if(dynamic_cast<C *>(p))
+	{
+		std::cout << "C" << std::endl;
+		return ;
+	}
+}
+
+Base * generate(void)
+{
+	srand(time(NULL));
+
+    Base *generated = NULL;
+    switch (std::rand() % 3) {
+        case 0:
+            generated = new A();
+            std::cout << "generated: A" << std::endl;
+            break;
+        case 1:
+            generated = new B();
+            std::cout << "generated: B" << std::endl;;
+            break;
+        case 2:
+            generated = new C();
+			std::cout << "generated: C" << std::endl;;
+            break;
+    }
+    return generated;
+}
+
+int		main(void)
+{
+	Base *p = generate();
+	std::cout << "reference" << std::endl;
+	identify(*p);
+	std::cout << "\npointer" << std::endl;
+	identify(p);
+	return (0);
+}
