480bd38233bff20025c40551907c49a208fb2818
yuki-katayama <katayu810@gmail.com>
2022-03-11 20:02:36 +0900

    fix: message

--- module04/ex03/Cure.cpp
+++ module04/ex03/Cure.cpp
@@ -33,7 +33,7 @@ AMateria *Cure::clone(void) const
 
 void Cure::use(ICharacter &target)
 {
-	std::cout << "* heals " << target.getName() << " wounds *" << std::endl;
+	std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
 }
 
 std::ostream &operator<<(std::ostream &output, Cure const &rhs)
